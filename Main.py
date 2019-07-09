718a4faf04fdcf7c0ecdacf15116dec2ec00400e
mattfreed <mwfreed@wpi.edu>
2019-07-09 12:34:49 -0400

    Add convolutional network for number detection

--- Main.py
+++ Main.py
@@ -3,11 +3,17 @@ import matplotlib.pyplot as plt
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
+from keras.models import Model
 from keras.layers import Dense
 from keras.optimizers import Adam
 from keras.utils.np_utils import to_categorical
 import random
-
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import  MaxPooling2D
+import requests
+from PIL import Image
+import cv2
 
 
 #Practice from the complete self driving car course - applied deep learning on Udemy.com
@@ -55,3 +61,31 @@ y_test = to_categorical(y_test, 10)
 
 X_train = X_train / 255
 X_test = X_test / 255
+
+def leNet_Model():
+    model = Sequential()
+    model.add(Conv2D(30, (5,5), input_shape = (28,28,1), activation = 'relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Conv2D(15, (3,3), activation = 'relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(500, activation = 'relu'))
+    model.add(Dense(num_classes ,activation = 'softmax'))
+    model.compile(Adam(lr=0.01), loss = 'categorical_crossentropy', metrics = ['accuracy'])
+    return model
+
+model = leNet_Model()
+print(model.summary())
+
+history = model.fit(X_train,y_train,epochs=10,validation_split=0.1,batch_size=400,verbose=1,shuffle=1)
+
+url = 'https://www.researchgate.net/profile/Jose_Sempere/publication/221258631/figure/fig1/AS:305526891139075@1449854695342/Handwritten-digit-2.png'
+response = requests.get(url, stream = True)
+img = Image.open(response.raw)
+img_array = np.asarray(img)
+resized = cv2.resize(img_array, (28,28))
+gray_scale = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+image = cv2.bitwise_not(gray_scale)
+plt.imshow(image,cmap = plt.get_cmap("gray"))
+print(image.shape)
+plt.show()
